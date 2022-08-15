75bcaa6c7150eea702ee436de55c5d467eb1bcb4
Munbear <pdwssf@gmail.com>
2022-08-15 22:56:54 +0900

    chatting app

---
+++ chatting_app/lib/screens/main_screen.dart
@@ -0,0 +1,53 @@
+import 'package:chatting_app/config/palette.dart';
+import 'package:flutter/material.dart';
+
+class LoginSignupScreen extends StatefulWidget {
+  const LoginSignupScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginSignupScreen> createState() => _LoginSignupScreenState();
+}
+
+class _LoginSignupScreenState extends State<LoginSignupScreen> {
+  bool isSignupScreen = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Palette.backgroundColor,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            right: 0,
+            left: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/red.jpg'),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              child: Container(
+                padding: EdgeInsets.only(top: 90, left: 20.0),
+                child: Column(
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        text: 'Welcome',
+                        style: TextStyle(
+                          letterSpacing: 1.0,
+                          fontSize: 25,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- weather_app/lib/screens/weather_screen.dart
+++ weather_app/lib/screens/weather_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:timer_builder/timer_builder.dart';
+import 'package:intl/intl.dart';
 
 class WeatherScreen extends StatefulWidget {
   WeatherScreen({this.parseWeatherData});
@@ -10,6 +13,11 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
+  String getSystemTime() {
+    var now = DateTime.now();
+    return DateFormat('h;mm a').format(now);
+  }
+
   String? cityName;
   int? temp;
   @override
@@ -30,24 +38,67 @@ class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                '$cityName',
-                style: TextStyle(fontSize: 30.0),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                '$temp',
-                style: TextStyle(fontSize: 30.0),
-              ),
-            ],
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Text(''),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(Icons.near_me),
+          onPressed: () {},
+          iconSize: 30.0,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.location_searching),
+            iconSize: 30.0,
           ),
+        ],
+      ),
+      body: Container(
+        child: Stack(
+          children: [
+            Image.asset(
+              'image/background.jpg',
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 150,
+                  ),
+                  Text(
+                    'Seoul',
+                    style: TextStyle(
+                      fontSize: 35.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      TimerBuilder.periodic(
+                        (Duration(minutes: 1)),
+                        builder: (context) {
+                          print('${getSystemTime()}');
+                          return Text(
+                            '${getSystemTime()}',
+                            style: GoogleFonts.lato(
+                                fontSize: 16, color: Colors.white),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
