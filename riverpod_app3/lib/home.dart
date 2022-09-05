40bff0b47c4596eed24c6da0e3344cf8bde4b69e
Munbear <pdwssf@gmail.com>
2022-09-06 01:05:12 +0900

    riverpod simple example 2

--- riverpod_app3/lib/home.dart
+++ riverpod_app3/lib/home.dart
@@ -2,13 +2,57 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_app3/count.dart';
 
-class MyHomePage extends ConsumerWidget {
+class CounterWidget extends ConsumerWidget {
+  const CounterWidget({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final value = ref.watch(valueProvider);
+    final counterRead = ref.read(myCounterStateNotifierProvider.notifier);
+    final counterState = ref.watch(myCounterStateNotifierProvider);
+
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Riverpod Counter widget'),
+      ),
       body: Center(
-        child: Text('Value: $value'),
+        child: Text(
+          'Value: $counterState',
+          style: const TextStyle(
+            fontSize: 48,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      floatingActionButton: Align(
+        alignment: Alignment.bottomRight,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              heroTag: '111',
+              onPressed: () => counterRead.increment(),
+              child: const Icon(
+                Icons.add,
+              ),
+            ),
+            const SizedBox(width: 10),
+            FloatingActionButton(
+              heroTag: '222',
+              onPressed: () => counterRead.decrement(),
+              child: const Icon(
+                Icons.remove,
+              ),
+            ),
+            const SizedBox(width: 10),
+            FloatingActionButton(
+              heroTag: '333',
+              onPressed: () => counterRead.initCount(),
+              child: const Icon(
+                Icons.refresh,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
