9befa781b7ddc5f1c5c5cf5a200f159965e81a75
Munbear <pdwssf@gmail.com>
2022-10-05 19:16:24 +0900

    studing mini player

--- study_mini_player_01/lib/first.dart
+++ study_mini_player_01/lib/first.dart
@@ -8,16 +8,41 @@ class FirstScreen extends StatefulWidget {
 }
 
 class _FirstScreenState extends State<FirstScreen> {
+  void onChangeBack() {
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: Container(
-            color: Colors.yellow,
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height - 10,
+      child: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: Colors.yellow,
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('First Screen'),
+                ),
+              ),
+            ),
           ),
-        )
-      ],
+          Expanded(
+            flex: 7,
+            child: GestureDetector(
+              child: Container(
+                color: Colors.grey,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
