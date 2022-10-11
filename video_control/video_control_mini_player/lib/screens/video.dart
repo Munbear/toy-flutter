852c886f4c754940b3f2f852ce704e28cfe63e35
Munbear <pdwssf@gmail.com>
2022-10-11 19:53:19 +0900

    video_player example

--- video_control/video_control_mini_player/lib/screens/video.dart
+++ video_control/video_control_mini_player/lib/screens/video.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoScreen extends StatefulWidget {
   const VideoScreen({super.key});
@@ -8,13 +11,53 @@ class VideoScreen extends StatefulWidget {
 }
 
 class _VideoScreenState extends State<VideoScreen> {
+  late VideoPlayerController _controller;
+  late Future<void> _initializaVideoPlayerFuture;
+
+  @override
+  void initState() {
+    _controller = VideoPlayerController.asset('assets/example_video.mp4')
+      ..initialize().then((_) {
+        setState(() {});
+      });
+    _initializaVideoPlayerFuture = _controller.initialize();
+    // _controller.setLooping(true);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          SizedBox(
+            width: 300,
+            height: 300,
+            // VideoPlayerController가 초기화 하는 동안 로딩 스피너 보여주기 위해
+            // FutureBuilder 위젯 사용
+            child: FutureBuilder(
+              future: _initializaVideoPlayerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  // 초기화가 끝나면 제공된 데이터를 사용해
+                  return AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    // 영상을 보여줌
+                    child: VideoPlayer(_controller),
+                  );
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ),
           Center(
             child: ElevatedButton(
               onPressed: () => Navigator.pop(context),
@@ -22,6 +65,25 @@ class _VideoScreenState extends State<VideoScreen> {
             ),
           ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // 재생/일시 중지 기능을 `setState` 호출로 감쌉니다. 이렇게 함으로써 올바른 아이콘이
+          // 보여집니다.
+          setState(() {
+            // 영상이 재생 중이라면, 일시 중지 시킵니다.
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              // 만약 영상이 일시 중지 상태였다면, 재생합니다.
+              _controller.play();
+            }
+          });
+        },
+        // 플레이어의 상태에 따라 올바른 아이콘을 보여줍니다.
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
       ),
     );
   }
